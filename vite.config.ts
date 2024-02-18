09bccd9525bc710240d9aa32dd3bac1544bd07aa
Wilson <77452536+wilson-kbs@users.noreply.github.com>
2024-02-18 20:11:46 +0100

    chore: add ci/cd to publish on github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
+const NODE_ENV = process.env.NODE_ENV;
+const DEMO_PREFIX = process.env.DEMO_PREFIX;
+
+function getBase() {
+  if (NODE_ENV === "demo") {
+    return DEMO_PREFIX || "/";
+  }
+  return "/";
+}
+
 export default defineConfig({
+  base: getBase(),
   plugins: [tsconfigPaths(), react()],
 });
